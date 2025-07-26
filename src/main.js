bd3732f7f3c1e28c395ba6d02cde56c85f7c4a47
kudaompq <kudaompq@foxmail.comm>
2025-07-26 17:46:56 +0800

    [init] 初始化项目

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Simple Vue3 Framework Entry Point
+console.log('Simple Vue3 启动中...')
+
+// 这里将是您的 Vue3 框架代码
+const app = document.getElementById('app')
+if (app) {
+  app.innerHTML = '<h1>Hello Simple Vue3!</h1>'
+}
